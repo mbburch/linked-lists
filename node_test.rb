dc1d7428c319e322e08ce9aedb25083fe64f71e8
MB Burch <marybethburch@gmail.com>
2015-07-22 20:23:21 -0600

    cleaned up node test

--- node_test.rb
+++ node_test.rb
@@ -6,14 +6,19 @@ require_relative 'node'
 
 class NodeTest < Minitest::Test
 
-  def test_it_knows_if_it_is_root
+  def test_it_returns_true_if_node_is_root
     node = Node.new
     assert node.root?
   end
 
-  def test_it_knows_if_it_is_tail
+  def test_it_returns_false_if_node_not_root
+    node = Node.new("peanuts")
+    refute node.root?
+  end
+
+  def test_it_returns_true_if_node_is_root
     node = Node.new("pizza")
-    refute node.head?
+    refute node.root?
     assert node.tail?
   end
 
