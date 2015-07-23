c49b44a935ef5edcd82f7392e45fb41f27b2f5bb
MB Burch <marybethburch@gmail.com>
2015-07-22 20:11:38 -0600

    updated test

--- linked_list_test.rb
+++ linked_list_test.rb
@@ -29,6 +29,10 @@ class LinkedListTest < Minitest::Test
     assert_equal "nachos", list.tail
   end
 
+  def test_returns_error_when_user_appends_empty_node
+    skip
+  end
+
   def test_empty_list_can_prepend_node
     list = LinkedList.new
     list.prepend("gum")
@@ -42,6 +46,7 @@ class LinkedListTest < Minitest::Test
     list.prepend("peanuts")
     assert_equal "peanuts", list.head
     assert_equal "popcorn", list.tail
+    assert_equal 2, list.count
   end
 
   def test_list_with_two_nodes_can_prepend_third_node
@@ -52,6 +57,10 @@ class LinkedListTest < Minitest::Test
     assert_equal "nachos", list.head
     assert_equal "popcorn", list.tail
     assert_equal 3, list.count
+  end
+
+  def test_returns_error_when_user_prepends_empty_node
+    skip
   end
 
   def test_it_inserts_a_node_at_given_index_in_empty_list
@@ -115,13 +124,15 @@ class LinkedListTest < Minitest::Test
     list.append("peanuts")
     list.append("apples")
     refute_equal "almonds", list.pop
+    refute list.includes?("apple")
+    assert list.includes?("almonds")
   end
 
   def test_it_counts_number_of_nodes_in_list_if_list_not_empty
     list = LinkedList.new
     list.append("popcorn")
     list.append("almonds")
-    list.append("peanuts")
+    list.prepend("peanuts")
     list.append("apples")
     expected = 4
     result = list.count
@@ -138,12 +149,17 @@ class LinkedListTest < Minitest::Test
 
   def test_it_returns_head_value_at_beginning_of_list
     list = LinkedList.new
-    list.append("popcorn")
     list.append("almonds")
     list.append("peanuts")
+    list.prepend("popcorn")
     list.append("apples")
     assert_equal "popcorn", list.head
     refute_equal "peanuts", list.head
+  end
+
+  def test_head_returns_nil_if_list_empty
+    list = LinkedList.new
+    assert_equal nil, list.head
   end
 
   def test_it_returns_tail_value_at_end_of_list
@@ -156,22 +172,28 @@ class LinkedListTest < Minitest::Test
     refute_equal "peanuts", list.tail
   end
 
+  def test_tail_returns_nil_if_list_empty
+    list = LinkedList.new
+    assert_equal nil, list.tail
+  end
+
   def test_it_finds_the_value_at_given_index
     list = LinkedList.new
     list.append("popcorn")
     list.append("almonds")
     list.append("peanuts")
-    list.insert(3, "popcorn")
-    assert_equal "popcorn", list.find_by_index(3)
+    list.append("popcorn")
+    assert_equal "peanuts", list.find_by_index(2)
   end
 
   def test_it_finds_position_of_first_occurrence_of_a_value
     list = LinkedList.new
     list.append("popcorn")
     list.append("almonds")
+    list.append("apples")
     list.append("peanuts")
     list.append("apples")
-    assert_equal 3, list.find_by_value("apples")
+    assert_equal 2, list.find_by_value("apples")
   end
 
   def test_it_removes_the_value_at_specified_index
@@ -200,6 +222,12 @@ class LinkedListTest < Minitest::Test
   def test_it_knows_if_the_list_is_empty
     list = LinkedList.new
     assert list.empty?
+  end
+
+  def test_it_returns_false_if_list_is_not_empty
+    list = LinkedList.new
+    list.insert(0, "peaches")
+    refute list.empty?
   end
 
   def test_it_finds_the_distance_between_two_nodes
